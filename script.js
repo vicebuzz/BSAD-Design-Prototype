c11c6f09fc3afecb2e4914ec5fdc945c3b4c840c
vicebuzz <vicebuzz@Mr-Lucky-Charmer.local>
2023-05-11 10:53:45 +0100

    changed styling and finished with main screen for now

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict'
+
+let searchButton = document.querySelector('#search-icon-button')
+searchButton.addEventListener('click', function () {
+    alert("Your search didn't match anything")
+})
